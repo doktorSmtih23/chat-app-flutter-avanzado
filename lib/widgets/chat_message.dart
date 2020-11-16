acdfc9ac145560a24946b486e6e4b622aefb6936
doktorSmtih23 <70076106+doktorSmtih23@users.noreply.github.com>
2020-11-16 11:41:31 -0500

    Add files via upload

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,10 +1,10 @@
 import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/painting.dart';
 
 import 'package:flutter_chat_bubble/bubble_type.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
+
 import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_8.dart';
 
 class ChatMessage extends StatelessWidget {
@@ -28,7 +28,9 @@ class ChatMessage extends StatelessWidget {
         sizeFactor:
             CurvedAnimation(parent: animationController, curve: Curves.easeOut),
         child: Container(
-          child: this.uid == authService.usuario.uid ? _myMessage() : _notMyMessage(),
+          child: this.uid == authService.usuario.uid
+              ? _myMessage()
+              : _notMyMessage(),
         ),
       ),
     );
@@ -37,13 +39,20 @@ class ChatMessage extends StatelessWidget {
   Widget _myMessage() {
     return ChatBubble(
       clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
-      alignment: Alignment.centerRight,
-      margin: EdgeInsets.only(top: 10),
-      backGroundColor: Colors.orange[400],
-        child: Container(
-        padding: EdgeInsets.all(8.0),
-        child: Text(this.texto, style: TextStyle(color: Colors.white, fontSize:16),
+      alignment: Alignment.topRight,
+      margin: EdgeInsets.only(top: 10, bottom: 3),
+      backGroundColor: Colors.orange,
+      child: Container(
+        margin: EdgeInsets.only(bottom: 2, left: 5, right: 5),
+        padding: EdgeInsets.all(3.0),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.white),
         ),
+        // decoration: BoxDecoration(
+        //   color: Color(0xff4d9ef6),
+        //   borderRadius: BorderRadius.circular(20),
+        // ),
       ),
     );
   }
@@ -52,11 +61,14 @@ class ChatMessage extends StatelessWidget {
     return ChatBubble(
       clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
       alignment: Alignment.centerLeft,
-      margin: EdgeInsets.only(top: 10,bottom: 10),
-      backGroundColor: Colors.grey[400],
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+      backGroundColor: Colors.grey,
       child: Container(
-        padding: EdgeInsets.all(8.0),
-          child: Text(this.texto, style: TextStyle(color: Colors.white,fontSize:16),
+        margin: EdgeInsets.only(bottom: 2, left: 5, right: 5),
+        padding: EdgeInsets.all(3.0),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.black87),
         ),
       ),
     );
