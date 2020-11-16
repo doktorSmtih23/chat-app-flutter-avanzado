8e3ba1bdff92b2191cb8129ac149a77b15b1b392
doktorSmtih23 <70076106+doktorSmtih23@users.noreply.github.com>
2020-11-16 11:30:34 -0500

    Add files via upload

---
+++ lib/providers/push_notifications_provider.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationsProvider {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  static Future<dynamic> onBackgroundMessage(
+      Map<String, dynamic> message) async {
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data'];
+    }
+
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      final dynamic notification = message['notification'];
+    }
+
+    // Or do other work.
+  }
+
+  get initNotifications async {
+    await _firebaseMessaging.requestNotificationPermissions();
+
+    final token = await _firebaseMessaging.getToken();
+    print('======FCM Token=====');
+    print('$token');
+
+    _firebaseMessaging.configure(
+        onMessage: onMessage,
+        onBackgroundMessage: onBackgroundMessage,
+        onLaunch: onLaunch,
+        onResume: onResume);
+  }
+
+  Future<dynamic> onMessage(Map<String, dynamic> message) async {
+    print('=====onMessage=====');
+    print('message:$message');
+  }
+
+  Future<dynamic> onLaunch(Map<String, dynamic> message) async {
+    print('=====onLaunch=====');
+    print('message:$message');
+  }
+
+  Future<dynamic> onResume(Map<String, dynamic> message) async {
+    print('=====onResuem=====');
+    print('message:$message');
+  }
+}
