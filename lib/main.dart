0de914df03264f95f016daa7866ab58ad8d7999e
doktorSmtih23 <70076106+doktorSmtih23@users.noreply.github.com>
2020-11-16 11:31:25 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/providers/push_notifications_provider.dart';
 import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -6,17 +7,29 @@ import 'package:provider/provider.dart';
 
 import 'services/chat_service.dart';
 import 'services/socket_service.dart';
- 
+
 void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    final pushProvider = new PushNotificationsProvider();
+    pushProvider.initNotifications;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)=>AuthService()),
-        ChangeNotifierProvider(create: (_)=>SocketService()),
-        ChangeNotifierProvider(create: (_)=>ChatService()),
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => SocketService()),
+        ChangeNotifierProvider(create: (_) => ChatService()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
